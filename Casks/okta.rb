e8be894063df8e49cbe7977424c52bb9fb92853e
Brian Demers <bdemers@apache.org>
2020-11-20 15:40:06 -0500

    Update to the Okta CLI 0.7.1

--- Casks/okta.rb
+++ Casks/okta.rb
@@ -1,6 +1,6 @@
 cask 'okta' do
-  version '0.7.0'
-  sha256 '70b3bb85c533c6a3574e8f9f8573895c70e0f9bd5608bc0da7e5e5cc786a57a4'
+  version '0.7.1'
+  sha256 '048a0bf66e42889b5496f5483f9a616ba1c987ff42c0d13a25c37e9287162c76'
 
   url "https://github.com/oktadeveloper/okta-cli/releases/download/okta-cli-tools-#{version}/okta-cli-macos-#{version}-x86_64.zip"
   appcast 'https://github.com/oktadeveloper/okta-cli/releases.atom'
