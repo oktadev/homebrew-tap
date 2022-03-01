be18f942f9bd4f19823d320df392fc5e71d777e1
Brian Demers <bdemers@apache.org>
2022-02-28 20:57:51 -0500

    Update version to 0.10.0

--- Casks/okta.rb
+++ Casks/okta.rb
@@ -1,11 +1,11 @@
 cask 'okta' do
-  version '0.9.0'
-  sha256 '04d7836e2075e8a1f2a49a4303b41ef7bff4d152a9fd48be609aec3b106f6276'
+  version '0.10.0'
+  sha256 '3a82e5dc6bfd7c6415706b92da8b61d04ea45d47373aece3acf7697fa3b25949'
 
   url "https://github.com/okta/okta-cli/releases/download/okta-cli-tools-#{version}/okta-cli-macos-#{version}-x86_64.zip"
   appcast 'https://github.com/okta/okta-cli/releases.atom'
   name 'okta'
-  homepage 'https://github.com/okta/okta-cli'
+  homepage 'https://cli.okta.com/'
 
   binary 'okta'
 end
