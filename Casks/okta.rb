35757034fcc3b554751054555ac61f199138f4cf
Guillaume Winter <DocEmmetBrown@users.noreply.github.com>
2023-07-17 15:47:07 +0200

    replace appcast by livecheck

--- Casks/okta.rb
+++ Casks/okta.rb
@@ -3,7 +3,7 @@ cask 'okta' do
   sha256 '3a82e5dc6bfd7c6415706b92da8b61d04ea45d47373aece3acf7697fa3b25949'
 
   url "https://github.com/okta/okta-cli/releases/download/okta-cli-tools-#{version}/okta-cli-macos-#{version}-x86_64.zip"
-  appcast 'https://github.com/okta/okta-cli/releases.atom'
+  livecheck 'https://github.com/okta/okta-cli/releases.atom'
   name 'okta'
   homepage 'https://cli.okta.com/'
 
